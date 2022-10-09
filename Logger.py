d0fc85b0794a8fa19c3fae05fef34609e6e6b7d5
Stephen <stephen.stengel@cwu.edu>
2022-10-09 15:25:49 -0700

    Added the loop that prints the time log to file.

--- Logger.py
+++ Logger.py
@@ -5,13 +5,40 @@
 #  
 #  Copyright 2022 Stephen Stengel <stephen.stengel@cwu.edu>
 
-
+import datetime
+import time
 
 
 def main(args):
+	outFileName = "time-to-death.txt"
+	timeToSleep = 1   # in seconds
+	
 	print("Hi!")
+	print("This program will run until canceled with ctrl-c or the" \
+			+ " battery dies.")
+	print("The total runtime of the battery will be recorded in a " \
+			+ "file named: \n" + outFileName)
+	
+	runLogger(outFileName, timeToSleep)
 
 	return 0
+
+
+def runLogger(outFileName, timeToSleep):
+	startingSeconds = time.time()
+	
+	while(True):
+		with open(outFileName, "w") as timeLog:
+			outputLine = "Last clock time: "
+			outputLine += str(time.asctime(time.localtime())) + "\n"
+			outputLine += "Elapsed time in seconds: "
+			outputLine += str(int(time.time() - startingSeconds)) + "\n"
+			
+			timeLog.write(outputLine)
+			timeLog.flush() ## This is needed to write to the file before closing the file.
+
+			time.sleep(timeToSleep)
+
 
 if __name__ == '__main__':
 	import sys

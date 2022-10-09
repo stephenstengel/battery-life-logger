17a16e30877bfc0bc16b180413b3861c97841b0e
Stephen <stephen.stengel@cwu.edu>
2022-10-09 14:13:29 -0700

    Initial commit.

---
+++ Logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Logger.py
+#  
+#  Copyright 2022 Stephen Stengel <stephen.stengel@cwu.edu>
+
+
+
+
+def main(args):
+	print("Hi!")
+
+	return 0
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
